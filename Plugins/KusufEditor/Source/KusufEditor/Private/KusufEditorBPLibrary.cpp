44bdb5e9eafd34b62b05fc0d488140e53a15dcc1
Rafael-Omalian <pro.adrien.paul@gmail.com>
2025-02-19 21:37:47 +0100

    Plugin: Creation d'un outil pour remplacer un cube classique par ceux de Kusuf
    KusufEditor is born !

---
+++ Plugins/KusufEditor/Source/KusufEditor/Private/KusufEditorBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "KusufEditorBPLibrary.h"
+#include "KusufEditor.h"
+
+UKusufEditorBPLibrary::UKusufEditorBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UKusufEditorBPLibrary::KusufEditorSampleFunction(float Param)
+{
+	return -1;
+}
+
---
+++ Source/GamagoraJam25/GamagoraJam25.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GamagoraJam25.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GamagoraJam25, "GamagoraJam25" );
